3e38278fc2e120fa96982482dcf319e808bfb5ae
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2023-11-05 11:53:57 -0500

    v7

--- Backend/src/routers/index.routes.js
+++ Backend/src/routers/index.routes.js
@@ -3,7 +3,7 @@ import Product from "../models/Product";
 import Category from "../models/Categories";
 import Order from "../models/Order";
 import Customer from "../models/Customer";
-import OrderDetails from '../models/OrderDetails'
+import OrderDetails from "../models/OrderDetails";
 
 const router = Router();
 
@@ -11,6 +11,7 @@ router.get("/", (req, res) => {
   res.send("backend");
 });
 
+/* add */
 router.get("/product/add", async (req, res) => {
   res.send("saved");
 });
@@ -34,6 +35,25 @@ router.get("/customer/add", async (req, res) => {
   res.send("saved");
 });
 
+router.post("/openOrder", async (req, res) => {
+  try {
+    Order.create(req.body);
+    res.send("saved");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/orderDetails", async (req, res) => {
+  try {
+    OrderDetails.create(req.body);
+    res.send("saved");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/* listar */
 router.get("/product", async (req, res) => {
   const product = await Product.find();
   res.json(product);
@@ -44,6 +64,7 @@ router.get("/categories", async (req, res) => {
   res.json(categories);
 });
 
+/* actualizar */
 router.put("/productUpdate/:productID", async (req, res) => {
   try {
     const productNew = req.body;
@@ -64,15 +85,35 @@ router.put("/productUpdate/:productID", async (req, res) => {
   }
 });
 
-router.post("/openOrder", async (req, res) => {
+router.put("/detailsUpdate/:productID/:orderID", async (req, res) => {
   try {
-    Order.create(req.body);
-    res.send("saved");
+    const productNew = req.body;
+    const orderID = req.params.orderID;
+    const productID = req.params.productID;
+
+    const filter = {
+      orderID: orderID,
+      productID: productID,
+    };
+
+    const options = { new: true };
+
+    const resNew = await OrderDetails.findOneAndUpdate(
+      filter,
+      productNew,
+      options
+    );
+
+    if (!resNew) {
+      return res.status(404).json({ mensaje: "Producto no encontrado" });
+    }
+
+    res.json("Detalle actualizado");
   } catch (error) {
     console.log(error);
   }
 });
-
+/* buscar */
 router.get("/orderSearchBycustomerID/:customerID", async (req, res) => {
   try {
     const customerID = req.params.customerID;
@@ -91,12 +132,59 @@ router.get("/orderSearchBycustomerID/:customerID", async (req, res) => {
   }
 });
 
-router.post("/orderDetails", async (req, res) => {
+router.get("/orderDetails/:orderID", async (req, res) => {
   try {
-    OrderDetails.create(req.body);
-    res.send("saved");
+    const orderID = req.params.orderID;
+
+    const filter = { orderID: orderID };
+
+    const resNew = await OrderDetails.find(filter);
+
+    if (!resNew) {
+      return res.json(null);
+    }
+
+    res.json(resNew);
   } catch (error) {
-    console.log(error);
+    res.json(error);
+  }
+});
+
+router.get("/product/:productID", async (req, res) => {
+  try {
+    const productID = req.params.productID;
+
+    const filter = { productID: productID };
+
+    const resNew = await Product.find(filter);
+
+    if (!resNew) {
+      return res.json(null);
+    }
+
+    res.json(resNew);
+  } catch (error) {
+    res.json(error);
+  }
+});
+/* 
+eliminar */
+router.delete("/orderDetailsDelete/:productID", async (req, res) => {
+  try {
+    const productID = req.params.productID;
+
+    const filter = { productID: productID };
+
+    // Utiliza el método findOneAndDelete para eliminar un producto
+    const result = await OrderDetails.findOneAndDelete(filter);
+
+    if (!result) {
+      return res.status(404).json({ mensaje: "Producto no encontrado" });
+    }
+
+    res.json({ mensaje: "Producto eliminado correctamente" });
+  } catch (error) {
+    res.status(500).json({ mensaje: "Error al eliminar el producto", error: error });
   }
 });
 
--- frontend/src/components/ProductList.vue
+++ frontend/src/components/ProductList.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container mt-5 bg-white p-3">
-
     <h1 class="pb-3">Tu carrito</h1>
     <div class="row table-header pb-3">
       <div class="col-3">Nombre</div>
@@ -8,19 +7,55 @@
       <div class="col-3">Precio</div>
     </div>
 
-    <div class="row m-2 pt-2 rounded-corner" v-for="product in products" :key="product._id" style="background-color: #D9D9D9;">
+    <div
+      class="row m-2 pt-2 rounded-corner"
+      v-for="product in products"
+      :key="product._id"
+      style="background-color: #d9d9d9"
+    >
       <div class="col-3">{{ product.name }}</div>
-      <div class="col-3">{{ product.quantity }}</div>
+      <div class="col-3">
+        <span v-if="!product.editMode">{{ product.quantity }}</span>
+        <input
+          v-else
+          type="number"
+          v-model="product.editQuantity"
+          min="0"
+          step="1"
+          required
+        />
+      </div>
       <div class="col-3">{{ product.unitPrice }}</div>
       <div class="col-3 mb-2">
-        <button class="btn btn-outline-danger me-2">Eliminar</button>
-        <button class="btn btn-outline-dark">Editar</button>
+        <button
+          class="btn btn-outline-danger me-2"
+          @click="deleteProduct(product)"
+        >
+          Eliminar
+        </button>
+        <button class="btn btn-outline-dark" @click="editProduct(product)">
+          Editar
+        </button>
+        <button
+          class="btn btn-success"
+          @click="saveProduct(product)"
+          v-if="product.editMode"
+        >
+          Guardar
+        </button>
+        <button
+          class="btn btn-danger"
+          @click="cancelEdit(product)"
+          v-if="product.editMode"
+        >
+          Cancelar
+        </button>
       </div>
     </div>
     <div class="container d-flex align-items-center justify-content-end p-2">
       <div class="m-5 p-2">
         <p class="text-primary">Total</p>
-        <p>$ 0.00</p>
+        <p>{{ "$ " + total.toFixed(2) }}</p>
       </div>
       <button class="btn btn-primary">Comprar</button>
     </div>
@@ -33,8 +68,8 @@ export default {
     return {
       products: [],
       baseUrl: "http://localhost:3000",
-      perPage: 10,
-      pages: [],
+      total: 0,
+      order: [],
     };
   },
   created() {
@@ -42,9 +77,103 @@ export default {
   },
   methods: {
     async getProducts() {
-      const res = await this.axios.get(`${this.baseUrl}/product`);
-      console.log(res);
-      this.products = res.data;
+      this.order = (
+        await this.axios.get(`${this.baseUrl}/orderSearchBycustomerID/${"01"}`)
+      ).data;
+
+      const details = (
+        await this.axios.get(
+          `${this.baseUrl}/orderDetails/${this.order.orderID}`
+        )
+      ).data;
+
+      for (let index = 0; index < details.length; index++) {
+        const producto = (
+          await this.axios.get(
+            `${this.baseUrl}/product/${details[index].productID}`
+          )
+        ).data;
+        this.products.push({
+          productID: producto[0].productID,
+          name: producto[0].name,
+          quantity: details[index].quantity,
+          unitPrice: "$ " + producto[0].unitPrice,
+        });
+
+
+        this.total += (producto[0].unitPrice * details[index].quantity);
+      }
+    },
+    editProduct(product) {
+      // Activa el modo de edición para el producto
+      product.editMode = true;
+      // Guarda la cantidad actual para poder restaurarla en caso de cancelación
+      product.editQuantity = product.quantity;
+    },
+    async saveProduct(product) {
+      if (
+        isNaN(product.editQuantity) ||
+        product.editQuantity <= 0 ||
+        product.editQuantity % 1 != 0
+      ) {
+        alert("Ingresa una cantidad válida para agregar al carrito.");
+        return;
+      }
+
+      let editQuantity = (
+        await this.axios.get(`${this.baseUrl}/product/${product.productID}`)
+      ).data;
+      if (product.quantity < product.editQuantity)
+        editQuantity[0].unitStock -= Math.abs(
+          product.quantity - product.editQuantity
+        );
+      else
+        editQuantity[0].unitStock += Math.abs(
+          product.quantity - product.editQuantity
+        );
+      await this.axios.put(
+        `${this.baseUrl}/productUpdate/${product.productID}`,
+        editQuantity[0]
+      );
+
+      const detailsUpdate = {
+        orderID: this.order.orderID,
+        productID: product.productID,
+        unitPrice: product.unitPrice.replace("$", ""),
+        quantity: product.editQuantity,
+      };
+      await this.axios.put(
+        `${this.baseUrl}/detailsUpdate/${product.productID}/${this.order.orderID}`,
+        detailsUpdate
+      );
+
+      // Guarda la nueva cantidad y desactiva el modo de edición
+      product.quantity = product.editQuantity;
+      product.editMode = false;
+    },
+    cancelEdit(product) {
+      // Cancela la edición y restaura la cantidad original
+      product.editQuantity = product.quantity;
+      product.editMode = false;
+    },
+    async deleteProduct(product) {
+      let editQuantity = (
+        await this.axios.get(`${this.baseUrl}/product/${product.productID}`)
+      ).data;
+      editQuantity[0].unitStock += product.quantity;
+      await this.axios.put(
+        `${this.baseUrl}/productUpdate/${product.productID}`,
+        editQuantity[0]
+      );
+
+      await this.axios.delete(
+        `${this.baseUrl}/orderDetailsDelete/${product.productID}`
+      );
+
+      
+      this.products = []
+      await this.getProducts();
+      
     },
   },
 };
