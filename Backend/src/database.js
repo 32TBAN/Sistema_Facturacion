2f2043fa20ef4d38fdadcd314ae4681af3eafb0d
Kevin Guanoluisa <94403249+32TBAN@users.noreply.github.com>
2024-12-16 08:08:09 -0500

    Update database.js

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://estebanguanoluisa55:MI3deenero@cluster0.xun82if.mongodb.net/api-db?retryWrites=true&w=majority"
+    "mongodb+srv://"
   )
   .then(console.log("se a conectado"))
   .catch((err) => console.log(err));
